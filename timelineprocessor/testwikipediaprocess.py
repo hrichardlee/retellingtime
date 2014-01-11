7e855a8261d371d3b73252ea392b4b15701e3415
Hyunho Richard Lee <hrichardlee@gmail.com>
2014-01-11 13:05:06 -0800

    Further refactored extraction code and added robustness

--- timelineprocessor/testwikipediaprocess.py
+++ timelineprocessor/testwikipediaprocess.py
@@ -1,57 +1,40 @@
 # -*- coding: UTF-8 -*-
 
 import wikipedia
+from testdata import *
 import wikipediaprocess
 from bs4 import BeautifulSoup
 import unittest
 import json
 from pprint import pprint
 
-htmlone = """<h2><span class="mw-headline" id="Progressive_Era_.281890.E2.80.931919.29"><a href="/wiki/Progressive_Era" title="Progressive Era">Progressive Era</a> (1890–1919)</span><span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/w/index.php?title=Timeline_of_United_States_inventions_(1890%E2%80%931945)&amp;action=edit&amp;section=1" title="Edit section: Progressive Era (1890–1919)">edit</a><span class="mw-editsection-bracket">]</span></span></h2>
-<p><b>1890 <a href="/wiki/Stop_sign" title="Stop sign">Stop sign</a></b></p>
-<p>A stop sign is a traffic sign, ...<sup id="cite_ref-13" class="reference"><a href="#cite_note-13"><span>[</span>13<span>]</span></a></sup></p>
-<p><b>1890 <a href="/wiki/Tabulating_machine" title="Tabulating machine">Tabulating machine</a></b></p>
-<p>The tabulating machine is an ...</p>
-<h2><span class="mw-headline" id="Progressive_Era_.281890.E2.80.931919.29"><a href="/wiki/Progressive_Era" title="Progressive Era">Progressive Era</a> (1890–1919)</span><span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/w/index.php?title=Timeline_of_United_States_inventions_(1890%E2%80%931945)&amp;action=edit&amp;section=1" title="Edit section: Progressive Era (1890–1919)">edit</a><span class="mw-editsection-bracket">]</span></span></h2>
-<p><b>1890 <a href="/wiki/Shredded_wheat" title="Shredded wheat">Shredded wheat</a></b></p>
-<ul>
-<li>Shredded wheat is a type of breakfast cereal made from whole <a href="/wiki/Wheat" title="Wheat">wheat</a>. Shredded wheat also comes in a <i>frosted</i> variety,...</li>
-</ul>"""
-stringblocksone = [[u'<b>1890 <a href="/wiki/Stop_sign" title="Stop sign">Stop sign</a></b>',
-			u'A stop sign is a traffic sign, ...<sup class="reference" id="cite_ref-13"><a href="#cite_note-13"><span>[</span>13<span>]</span></a></sup>',
-			u'<b>1890 <a href="/wiki/Tabulating_machine" title="Tabulating machine">Tabulating machine</a></b>',
-			u'The tabulating machine is an ...'],
-			[u'<b>1890 <a href="/wiki/Shredded_wheat" title="Shredded wheat">Shredded wheat</a></b>',
-			u'Shredded wheat is a type of breakfast cereal made from whole <a href="/wiki/Wheat" title="Wheat">wheat</a>. Shredded wheat also comes in a <i>frosted</i> variety,...'			]]
-htmltwo = """<p><b>1890 <a href="/wiki/Stop_sign" title="Stop sign">Stop sign</a></b></p>
-<p>A stop sign is a traffic sign, ...<sup id="cite_ref-13" class="reference"><a href="#cite_note-13"><span>[</span>13<span>]</span></a></sup></p>
-<p><b>1890 <a href="/wiki/Tabulating_machine" title="Tabulating machine">Tabulating machine</a></b></p>
-<p>The tabulating machine is an ...</p>"""
-stringblockstwo = [[u'<b>1890 <a href="/wiki/Stop_sign" title="Stop sign">Stop sign</a></b>',
-			u'A stop sign is a traffic sign, ...<sup class="reference" id="cite_ref-13"><a href="#cite_note-13"><span>[</span>13<span>]</span></a></sup>',
-			u'<b>1890 <a href="/wiki/Tabulating_machine" title="Tabulating machine">Tabulating machine</a></b>',
-			u'The tabulating machine is an ...']]
 
-class TestExtractTimeline(unittest.TestCase):
-	def testExtractTimeline(self):
-		data = wikipediaprocess.extractTimeline(wikipedia.page('Timeline of particle physics'))
-		print data
-		with open('particlephysics.json', 'w') as f:
-			f.write(json.dumps(data))
 
-class TestStringsToEvents(unittest.TestCase):
+class TestwpPageToJson(unittest.TestCase):
 	def testOne(self):
-		pprint(wikipediaprocess.stringsToEvents(stringblocksone))
+		pprint(wikipediaprocess.wpPageToJson("timeline of particle physics"))
 
-class TestGetStringBlocks(unittest.TestCase):
+class TestStringBlocksToEvents(unittest.TestCase):
+	def p(self, data):
+		pprint([str(x["date"]) + ":::" + x["content"][:60] for x in data])
+	def testOne(self):
+		self.p(wikipediaprocess.stringBlocksToEvents(stringblocksone))
+	def testTwo(self):
+		article = BeautifulSoup(html_particlephysics)
+		self.p(wikipediaprocess.stringBlocksToEvents(wikipediaprocess.htmlToStringBlocks(article)))
+	def testThree(self):
+		article = BeautifulSoup(html_modernhist)
+		self.p(wikipediaprocess.stringBlocksToEvents(wikipediaprocess.htmlToStringBlocks(article)))
+
+class TesthtmlToStringBlocks(unittest.TestCase):
 	def testOne(self):
 		html = BeautifulSoup(htmlone)
-		strings = wikipediaprocess.getStringBlocks(html)
+		strings = wikipediaprocess.htmlToStringBlocks(html)
 		self.assertEqual(strings, stringblocksone)
 
 	def testTwo(self):
 		html = BeautifulSoup(htmltwo)
-		strings = wikipediaprocess.getStringBlocks(html)
+		strings = wikipediaprocess.htmlToStringBlocks(html)
 		self.assertEqual(strings, stringblockstwo)
 
 class TestFindDate(unittest.TestCase):
@@ -64,28 +47,28 @@ class TestFindDate(unittest.TestCase):
 		self.remainderthreelayers = '<a href="/wiki/Stop_sign" title="Stop sign">Stop sign</a>blergh'
 
 	def testOne(self):
-		(x, y) = wikipediaprocess.getFirstPart(BeautifulSoup(self.onelayer))
+		(x, y) = wikipediaprocess.getFirstTextNode(BeautifulSoup(self.onelayer))
 		self.assertEqual(x, '1890 ')
 		self.assertEqual(y, self.remainder)
 		(x, y) = wikipediaprocess.findDate(self.onelayer)
 		self.assertEqual(x, 1890)
 		self.assertEqual(y, self.remainder)
 	def testTwo(self):
-		(x, y) = wikipediaprocess.getFirstPart(BeautifulSoup(self.nolayer))
+		(x, y) = wikipediaprocess.getFirstTextNode(BeautifulSoup(self.nolayer))
 		self.assertEqual(x, '1890 ')
 		self.assertEqual(y, self.remainder)
 		(x, y) = wikipediaprocess.findDate(self.nolayer)
 		self.assertEqual(x, 1890)
 		self.assertEqual(y, self.remainder)
 	def testThree(self):
-		(x, y) = wikipediaprocess.getFirstPart(BeautifulSoup(self.twolayers))
+		(x, y) = wikipediaprocess.getFirstTextNode(BeautifulSoup(self.twolayers))
 		self.assertEqual(x, '1890 ')
 		self.assertEqual(y, self.remainder)
 		(x, y) = wikipediaprocess.findDate(self.twolayers)
 		self.assertEqual(x, 1890)
 		self.assertEqual(y, self.remainder)
 	def testFour(self):
-		(x, y) = wikipediaprocess.getFirstPart(BeautifulSoup(self.threelayers))
+		(x, y) = wikipediaprocess.getFirstTextNode(BeautifulSoup(self.threelayers))
 		self.assertEqual(x, '1890 ')
 		self.assertEqual(y, self.remainderthreelayers)
 		(x, y) = wikipediaprocess.findDate(self.threelayers)
--- timelineprocessor/wikipediaprocess.py
+++ timelineprocessor/wikipediaprocess.py
@@ -10,105 +10,115 @@ from bs4 import BeautifulSoup
 
 import pdb
 
-def jsonFromPage(title):
-	"""Takes the title of a timeline page on Wikipedia and
-	outputs it to a json file
-	"""
-	return json.dumps(extractTimeline(wikipedia.page(title)))
 
-def getStringBlocks(html, 
-	blockElements = set(["address", "article", "aside", "blockquote", "dd", "div", "dl", "p", "ol", "ul", "li"]),
-	headerElements = set(["h2", "h3"])):
-	"""Given an html element, returns a list of list of strings. Each section, demarcated
-	by a headerElement is a list of strings. Each block element in html is a string.
-	Empty lines are not retained.
+def wpPageToJson(title):
+	"""Takes the title of a timeline page on Wikipedia and outputs it to a
+	json file
+	"""
+	article = BeautifulSoup(wikipedia.page(title).html())
+
+	return json.dumps(stringBlocksToEvents(htmlToStringBlocks(article)))
+
+
+def htmlToStringBlocks(html, 
+	blockElements = set(["address", "article", "aside", "blockquote", "section", "dd", "div", "dl", "p", "ol", "ul", "li"]),
+	headerElements = ["h2", "h3"]):
+	"""Given an html element as a BeautifulSoup, returns a list of string
+	blocks. Each string block represents a section in the html demarcated by a
+	header element. Each string block is {heading, lines}. Lines is a list of
+	strings where each block element in html is a string. Heading is a list
+	where the nth element represents the nth subheading applicable to the
+	current section. Empty lines are not retained.
 	"""
 
-	def closeStringBlock(sb, s):
+	def closeString(sb, s):
 		s = s.strip()
 		if s:
-			sb.append(s)
+			sb["lines"].append(s)
 
-	def closeStringBlocks(sbs, sb, s):
-		closeStringBlock(sb, s)
-		if sb:
+	def closeStringBlock(sbs, sb, s):
+		closeString(sb, s)
+		if sb["lines"]:
 			sbs.append(sb)
 
 	stringBlocks = []
-	currStringBlock = []
+	currHeading = [""]
+	currStringBlock = {"lines": [], "heading": currHeading}
 	currString = ""
 	for el in html.children:
 		if el.name in headerElements:
-			closeStringBlocks(stringBlocks, currStringBlock, currString)
-			currStringBlock = []
+			# close the previous block
+			closeStringBlock(stringBlocks, currStringBlock, currString)
+
+			headingIndex = headerElements.index(el.name)
+			currHeading = currHeading[:headingIndex]
+			if len(currHeading) < headingIndex:
+				currHeading += [""] * (headingIndex - len(currHeading)) 
+			# heading is usually under mw-headline, but sometimes is not
+			currHeading.append(el.find(class_='mw-headline').text if el.find(class_='mw-headline') else el.get_text())
+			currStringBlock = {"lines": [], "heading": currHeading}
 		elif el.name in blockElements:
-			closeStringBlock(currStringBlock, currString)
+			closeString(currStringBlock, currString)
 			currString = ""
-			# assumes no header elements in child elements
-			# TODO deal with headers better, and incorporate information
-			childStringBlocks = getStringBlocks(el)
+			# assumes no header elements under block elements
+			childStringBlocks = htmlToStringBlocks(el)
 			if childStringBlocks:
-				currStringBlock += childStringBlocks[0]
+				currStringBlock["lines"] += childStringBlocks[0]["lines"]
 		else:
 			currString += unicode(el)
 
-	closeStringBlocks(stringBlocks, currStringBlock, currString)
+	closeStringBlock(stringBlocks, currStringBlock, currString)
 
 	return stringBlocks
 
 
-def stringsToEvents(stringBlocks):
-	"""Given a set of sets of strings, returns a list of timeline events.
-	Expects that all strings are non-empty"""
+def stringBlocksToEvents(stringBlocks, lineBreak = "<br />",
+	ignoreSections = set(["", "Contents", "See also", "References"])):
+	"""Given a set of string blocks (as produced by htmlToStringBlocks,
+	expects that all strings are non-empty), returns a list of timeline
+	events. A timeline event is {date: number, content: string}
+	"""
 
 	currEvent = {}
 	events = []
 
-	lineBreak = "<br />"
-
 	for stringBlock in stringBlocks:
-		for string in stringBlock:
-			extract = findDate(string)
-			if extract:
-				if currEvent:
-					events.append(currEvent)
-				currEvent = {"date": extract[0], "content": extract[1]}
-			else:
-				if currEvent:
-					currEvent["content"] += lineBreak + string
+		if stringBlock["heading"][0] not in ignoreSections:
+			for string in stringBlock["lines"]:
+				extract = findDate(string)
+				if extract:
+					if currEvent:
+						events.append(currEvent)
+					currEvent = {"date": extract[0], "content": extract[1]}
+				else:
+					if currEvent:
+						currEvent["content"] += lineBreak + string
 	return events
 
 
-def extractTimeline(page):
-	"""Takes a timeline page from Wikipedia and turns it
-	into ... something?
-
-	page must be a wikipedia.WikipediaPage
-	""" 
-
-	article = BeautifulSoup(page.html())
-
-	return stringsToEvents(getStringBlocks(article))
-
-def getFirstPart(soup, remainder = []):
-	"""Given an html string, returns (firstPart, remainder) where firstPart is the 
+def getFirstTextNode(soup, remainder = []):
+	"""Given an html BeautifulSoup, returns (firstPart, remainder) where
+	firstPart is the first text node and remainder is everything after that
+	first text node
 	"""
 	try:
 		if soup.contents:
-			(firstPart, rem) = getFirstPart(soup.contents[0], soup.contents[1:])
-			return (firstPart, rem + "".join(unicode(s) for s in remainder))
+			(firstPart, innerRem) = getFirstTextNode(soup.contents[0], soup.contents[1:])
+			return (firstPart, innerRem + "".join(unicode(s) for s in remainder))
 		else:
 			return ("", "".join(unicode(s) for s in remainder))
 	except AttributeError:
 		return (soup, "".join(unicode(s) for s in remainder))
 
+
 def findDate(string):
-	"""Takes an html string, and returns (date, content) as a tuple.
-	For now, date is an int that represents the year. Negative numbers are B.C. and positive are A.D. years
+	"""Takes a string that contains html, and returns (date, content) as a
+	tuple. For now, date is an int that represents the year. Negative numbers
+	are B.C. and positive are A.D. years
 	"""
 
 	soup = BeautifulSoup(string)
-	(s, remainder) = getFirstPart(soup)
+	(s, remainder) = getFirstTextNode(soup)
 
 	# strip out all non-letter/digit characters from the beginning
 	m = re.search('^[^\d\w]+', s)
@@ -147,11 +157,14 @@ def findDate(string):
 		return None
 
 	# strip out any transition characters between the date and the content
-	# c stands for circa and is ignored for now
-	m2 = re.search(u'^[c\s\-ΓÇôΓÇö:\.]+', rem)
-	content = rem[m2.end():] + remainder
+	m2 = re.search(u'^[\s\-–—:\.]+', rem)
+	if m2:
+		content = rem[m2.end():] + remainder
+	else:
+		content = rem + remainder
 
 	return (date, content)
+
 
 def importanceString(s):
 	"""Given a content string for a timeline item, returns the importance score
@@ -167,13 +180,11 @@ def importanceString(s):
 
 	return float(imp) / float(n)
 
+
 def importancePage(p):
 	"""Given a wikipedia.WikipediaPage, returns the importance score
 	"""
 	return len(p.content)
 	#return (p.backlinkCount(), len(p.content), p.revCount())
 
-# TODO: add a relevance metric so that an entry like
-# 	1815 - [[William Prout]] [[hypothesizes]] that all matter is built up from [[hydrogen]], adumbrating the [[proton]]
-# in the timeline of particle physics doesn't get importance points
-# for "hypothesizes", as this doesn't have much to do with particle physics
+# TODO: add a relevance metric as a multiplier for the importance
