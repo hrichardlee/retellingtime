47e05cfab375744272717f58765b9d46e33574ee
Hyunho Richard Lee <hrichardlee@gmail.com>
2014-02-20 14:24:07 -0800

    Removed comments with example urls

--- ananke/urls.py
+++ ananke/urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ananke.views.home', name='home'),
     url(r'^timelinedata/', include('timelinedata.urls')),
     url(r'^viewer/', include('timelineviewer.urls')),
     url(r'^admin/', include(admin.site.urls)),
