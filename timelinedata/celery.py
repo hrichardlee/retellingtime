3ef60e619c021985dfd984b575e92f379c8629a8
Hyunho Richard Lee <hrichardlee@gmail.com>
2014-03-11 19:29:38 -0700

    Added async refresh and got rid of celery results

--- timelinedata/celery.py
+++ timelinedata/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retellingtime.settings')
 
-app = Celery('timelinedata', backend='amqp', broker='amqp://guest@localhost//')
+app = Celery('timelinedata', broker='amqp://guest@localhost//')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
